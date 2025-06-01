ccea0f9af65d0b9ed13a0ccbcbf55a87761d1973
kylee-ingalls <kylee.ingalls@gmail.com>
2025-06-01 17:39:59 -0500

    Injected the forecast HTML from JavaScript

--- src/index.js
+++ src/index.js
@@ -58,6 +58,32 @@ function refreshWeather(response) {
   
   let searchFormElement = document.querySelector("#search-form");
   searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+  function displayForecast() {
+
   
-  // Default city
-  searchCity("Oklahoma City");
+let days = ['Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+let forecastHtml = "";
+
+days.forEach(function (day) {
+  forecastHtml = 
+  forecastHtml +
+  `
+  <div class="weather-forecast-day">
+           <div class="weather-forecast-date">${day}</div>
+           <div class="weather-forecast-icon">🌤️</div>
+           <div class="weather-forecast-temperatures">
+             <div class="weather-forecast-temperature">
+               <strong>15º</strong>
+             </div>
+             <div class="weather-forecast-temperature">9º</div>
+           </div>
+         </div>
+         `;
+});
+let forecastElement = document.querySelector("#forecast");
+forecastElement.innerHTML = forecastHtml;
+  }
+   // Default city
+   searchCity("Oklahoma City");
+   displayForecast();
