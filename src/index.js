47247eaa4ec0a326bc4016442e7d5796a24a3b41
kylee-ingalls <kylee.ingalls@gmail.com>
2025-06-01 21:46:11 -0500

    Updated data using API

--- src/index.js
+++ src/index.js
@@ -58,6 +58,13 @@ function refreshWeather(response) {
     searchCity(searchInput.value);
   }
 
+  function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    return days[date.getDay()];
+  }
+
   let searchFormElement = document.querySelector("#search-form");
   searchFormElement.addEventListener("submit", handleSearchSubmit);
 
@@ -68,30 +75,31 @@ function refreshWeather(response) {
   }
 
   function displayForecast(response) {
-    console.log(response.data);
-let days = ['Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
 let forecastHtml = "";
 
-days.forEach(function (day) {
+response.data.daily.forEach(function (day, index) {
+  if (index < 5) {
   forecastHtml = 
   forecastHtml +
   `
   <div class="weather-forecast-day">
-           <div class="weather-forecast-date">${day}</div>
-           <div class="weather-forecast-icon">🌤️</div>
+           <div class="weather-forecast-date">${formatDay(day.time)}</div>
+        
+           <img src="${day.condition.icon_url}" class="weather-forecast-icon"/>
+          
            <div class="weather-forecast-temperatures">
              <div class="weather-forecast-temperature">
-               <strong>15º</strong>
+               <strong>${Math.round(day.temperature.maximum)}°</strong>
              </div>
-             <div class="weather-forecast-temperature">9º</div>
+             <div class="weather-forecast-temperature">${Math.round(day.temperature.minimum)}°</div>
            </div>
          </div>
          `;
+  }
 });
 let forecastElement = document.querySelector("#forecast");
 forecastElement.innerHTML = forecastHtml;
   }
    
   searchCity("Oklahoma City");
-
-  
